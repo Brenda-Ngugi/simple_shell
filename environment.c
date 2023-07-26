09240f685be2910ba82fa7464883a9a510636d22
ruthosoro <robinahruth@gmail.com>
2023-07-26 10:03:36 -0700

    fixed betty errors

--- environment.c
+++ environment.c
@@ -119,12 +119,12 @@ void is_current_path(char *path, general_t *info)
  **/
 void get_full_env(void)
 {
-        char **temp = environ;
-        int i;
+char **temp = environ;
+int i;
 
-        for (i = 0; temp[i] != NULL; i++)
-        {
-                print(temp[i]);
-                _putchar('\n');
-        }
+for (i = 0; temp[i] != NULL; i++)
+{
+print(temp[i]);
+_putchar('\n');
+}
 }
